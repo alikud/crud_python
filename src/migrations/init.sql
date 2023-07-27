02ded2bf41d06b57157f249f3d7d23e38b677feb
Aliaksei <72557052+alikud@users.noreply.github.com>
2023-07-27 12:43:54 +0500

    Update init.sql

--- src/migrations/init.sql
+++ src/migrations/init.sql
@@ -12,19 +12,9 @@ CREATE TABLE Books (
 
 -- Таблица "Авторы_Книги"
 CREATE TABLE Authors_Books (
+    ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     Author_ID UUID,
     Book_ID UUID,
-    PRIMARY KEY (Author_ID, Book_ID),
     FOREIGN KEY (Author_ID) REFERENCES Authors(ID),
-    FOREIGN KEY (Book_ID) REFERENCES Books(ID)
-);
-
--- Таблица "Бронирование_книг"
-CREATE TABLE Book_Reservations (
-    User_ID UUID,
-    Book_ID UUID,
-    Reservation_Date DATE,
-    PRIMARY KEY (User_ID, Book_ID),
-    FOREIGN KEY (User_ID) REFERENCES Users(ID),
     FOREIGN KEY (Book_ID) REFERENCES Books(ID)
 );
